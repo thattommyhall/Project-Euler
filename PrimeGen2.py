931ae253a2e34133293aa2851c10708489e0e703
thattommyhall <thattommyhall@gmail.com>
2010-10-18 14:13:28 +0100

    both genprimes correct

--- PrimeGen2.py
+++ PrimeGen2.py
@@ -6,33 +6,26 @@ def gen_primes():
     
     def update_composites(number,increment):
         heappush(composites,(number,increment))
-
     composites = [(4,2)]
 
     c = count(3)
     
     while 1:
-        print composites
+        #print composites
+        smallest,increment = heappop(composites)
+        update_composites(smallest+increment,increment)
+        if composites[0][0] == smallest:
+            continue
         next = c.next()
-        smallest,inc = heappop(composites)
-        while 1:
-            try:
-                nextsmallest,newinc = composites[0]
-            except IndexError:
-                break
-            if nextsmallest != smallest:
-                break
-            update_composites(nextsmallest+newinc,newinc)
-            smallest,inc = heappop(composites)
-            
         if next < smallest:
             yield next
             c.next()
             update_composites(2*next,next)
+        
+            
 
                         
 g=gen_primes()
     
 for i in range(100):
     print g.next()
-    raw_input()
